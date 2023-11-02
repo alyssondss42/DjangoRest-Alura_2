68386d1ced772020dc1b36a04ae5f58f537d0113
alyssondss42 <alyssondssoares@gmail.com>
2023-11-02 16:27:21 -0300

    Ajuste da validacao do numero de celular

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from clientes.models import Cliente
 from clientes.validators import *
 
+
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
@@ -15,7 +16,7 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({"rg": "O RG dete ver 9 dígitos."})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({"celular": "O celular deve ter 1 dígitos."})
+            raise serializers.ValidationError({"celular": "O número de celular deve seguir este modelo: 11 91234-4321"})
         return data
 
 
